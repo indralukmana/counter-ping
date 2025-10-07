2b48fa14621b32a37df86420cb32a316d24f7162
Indra Lukmana <indra@indralukmana.com>
2025-10-08 01:52:44 +0700

    feat: wireup gill react, rough integration

--- src/components/app-providers.tsx
+++ src/components/app-providers.tsx
@@ -1,13 +1,21 @@
 import { ThemeProvider } from '@/components/theme-provider'
 import { ReactQueryProvider } from './react-query-provider'
 import { SolanaProvider } from '@/components/solana/solana-provider'
+import { SolanaProvider as GillSolanaProvider } from '@gillsdk/react'
 import React from 'react'
+import { createSolanaClient } from 'gill'
 
 export function AppProviders({ children }: Readonly<{ children: React.ReactNode }>) {
+  const client = createSolanaClient({
+    urlOrMoniker: 'localnet',
+  })
+
   return (
     <ReactQueryProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <SolanaProvider>{children}</SolanaProvider>
+        <SolanaProvider>
+          <GillSolanaProvider client={client}>{children}</GillSolanaProvider>
+        </SolanaProvider>
       </ThemeProvider>
     </ReactQueryProvider>
   )
